58608096cccb1595c7bda28fbf6497896e753c80
Alex Moore <34859574+AleksMoore@users.noreply.github.com>
2020-08-21 19:00:36 +0300

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('.partners__slides').owlCarousel({
+        loop: true,
+        items: 5,
+        autoplay: true,
+        autoplayTimeout: 3000
+    });
+   });
+
+$(function() {
+    $('.reviews__slides').owlCarousel({
+        loop: true,
+        items: 2,
+        nav: true
+    });
+   });
+
+$(function() {
+    $('.info__slides').owlCarousel({
+        loop: true,
+        items: 2,
+        nav: true
+    });
+   });
