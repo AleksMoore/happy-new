176bf990c37001982fb552182f2ab334526d497a
Alex Moore <34859574+AleksMoore@users.noreply.github.com>
2020-10-02 15:35:36 +0300

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -59,3 +59,15 @@ $(function() {
         }
     });
    });
+
+   $(document).ready(function() {
+    $("a.header__link").click(function(){
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 1000,
+            easing: "swing"
+        });
+        return false;
+    });
+   });
